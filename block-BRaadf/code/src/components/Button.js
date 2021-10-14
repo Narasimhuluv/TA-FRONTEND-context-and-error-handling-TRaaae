bd79421e9da46e6ff97764eaa7afdd72a6a30579
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-10-14 17:27:53 +0530

    add files

--- block-BRaadf/code/src/components/Button.js
+++ block-BRaadf/code/src/components/Button.js
@@ -1,12 +1,39 @@
-import React from "react";
+// import React from "react";
+// import UserContext from "../Context";
 
-export default function SwitchButton({ isDarkMode, changeMode }) {
-  return (
-    <button
-      className={`btn ${isDarkMode ? "btn-dark" : "btn-light"}`}
-      onClick={changeMode}
-    >
-      {isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
-    </button>
-  );
+// export default function SwitchButton({ isDarkMode, changeMode }) {
+//   return (
+//     <button
+//       className={`btn ${isDarkMode ? "btn-dark" : "btn-light"}`}
+//       onClick={changeMode}
+//     >
+//       {isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+//     </button>
+//   );
+// }
+
+import React from "react";
+import UserContext from "../Context";
+
+class Banner extends React.Component {
+  
+  static contextType = UserContext;
+
+  render() {
+    let { isDarkMode } = this.context.data;
+    return (
+      <div
+        className={
+          isDarkMode
+            ? "message-dark message--banner message--success"
+            : "message message--banner message--success"
+        }
+      >
+        <h3 className="message-heading">This is a success banner!</h3>
+        <p>It celebrates that you have successfully completed a major task.</p>
+      </div>
+    );
+  }
 }
+
+export default Banner;
--- block-BRaadf/code/src/components/Header.js
+++ block-BRaadf/code/src/components/Header.js
@@ -1,4 +1,9 @@
+import {useContext} from 'react';
+import { UserContext } from '../Context';
+
 function Header({ isDarkMode }) {
+  const user = useContext(UserContext)
+  let isDarkMode = user.data.isDarkMode;
   return (
     <h1 className={`heading ${isDarkMode ? "heading-dark" : "heading-light"}`}>
       {isDarkMode ? "Dark Mode" : "Light Mode"}
